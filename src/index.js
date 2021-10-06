49a90397fa9294ea202dab90a7d15ed130a9f25a
Nathan Reidy <nathanjreidy@gmail.com>
2021-10-06 21:23:34 +1100

    Refactored schema and put in separate file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
-const typeDefs = `
-    type Query {
-        info: String!
-    }
-`;
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
+    info: () => "This is the API of a Hackernews Clone",
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
 });
 
