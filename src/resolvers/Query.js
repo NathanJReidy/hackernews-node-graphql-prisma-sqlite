80f98e1a187fbf28aefc3769f578dbd9337b0549
Nathan Reidy <nathanjreidy@gmail.com>
2021-10-11 21:08:53 +1100

    Added filtering, pagination and sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,23 @@
 const feed = async (parent, args, context) => {
-  return await context.prisma.link.findMany();
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return { links, count };
 };
 
 module.exports = {
